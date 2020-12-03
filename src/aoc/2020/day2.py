e55d1acf0789478b3b1ad00db1da2676e166bfe6
Tomas Aschan <1550920+tomasaschan@users.noreply.github.com>
2020-12-03 08:20:13 +0100

    Dec 2 ⭐⭐

---
+++ src/aoc/2020/day2.py
@@ -0,0 +1,36 @@
+import re
+from collections import defaultdict
+
+
+def read_input():
+    with open("./input/dec2.txt", "r") as f:
+        for line in f.readlines():
+            yield line
+
+
+rx = re.compile(r"(\d+)-(\d+) (\w): (\w+)")
+
+
+def part_1():
+    valid = 0
+    for line in read_input():
+        lo, hi, ch, pw = rx.match(line).groups()
+
+        letters = defaultdict(int)
+        for letter in pw:
+            letters[letter] += 1
+
+        if int(lo) <= letters.get(ch, 0) and letters.get(ch, 0) <= int(hi):
+            valid += 1
+
+    assert 622 == valid
+
+
+def part_2():
+    valid = 0
+    for line in read_input():
+        lo, hi, ch, pw = rx.match(line).groups()
+        if (pw[int(lo) - 1] == ch) ^ (pw[int(hi) - 1] == ch):
+            valid += 1
+
+    assert 263 == valid
