8c49eca37456a35f1c0ed8687cf6164a76fdbde7
Tomas Aschan <1550920+tomasaschan@users.noreply.github.com>
2020-12-03 08:20:56 +0100

    Dec 3 ⭐⭐

---
+++ src/aoc/2020/day3.py
@@ -0,0 +1,48 @@
+def read_map():
+    height = 0
+    width = 0
+
+    map = dict()
+
+    with open("input/dec3.txt", "r") as f:
+        for y, line in enumerate(f.readlines()):
+            y = int(y)
+            height = max(height, y)
+            for x, c in enumerate(line):
+                x = int(x)
+                width = max(width, x)
+                map[(x, y)] = c == "#"
+
+    return height, width, map
+
+
+def count_trees(map, h, w, dx, dy):
+    x, y = (0, 0)
+    trees = 0
+    while y <= h:
+        if map[(x % w, y)]:
+            trees += 1
+
+        x += dx
+        y += dy
+
+    return trees
+
+
+def part_1():
+    height, width, map = read_map()
+    assert height != 0
+    assert width != 0
+
+    assert 268 == count_trees(map, height, width, 3, 1)
+
+
+def part_2():
+    height, width, map = read_map()
+
+    p = 1
+    for dx, dy in ((1, 1), (3, 1), (5, 1), (7, 1), (1, 2)):
+        t = count_trees(map, height, width, dx, dy)
+        p *= t
+
+    assert p == 3093068400
