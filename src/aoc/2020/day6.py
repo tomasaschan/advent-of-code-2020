25639088750de986733b61daac186947d6150a2d
Tomas Aschan <1550920+tomasaschan@users.noreply.github.com>
2020-12-06 11:58:18 +0100

    Dec 6 ⭐⭐

---
+++ src/aoc/2020/day6.py
@@ -0,0 +1,34 @@
+import string
+
+with open("input/dec6.txt") as f:
+    input = f.read()
+
+
+def groups(input):
+    return input.split("\n\n")
+
+
+def part_1():
+    def sets(input):
+        for group in groups(input):
+            s = set()
+            for line in group.split("\n"):
+                s.update(line)
+
+            yield s
+
+    assert 6612 == sum(len(s) for s in sets(input))
+
+
+def part_2():
+    c = 0
+
+    for group in groups(input):
+        s = set(string.ascii_lowercase)
+
+        for person in group.splitlines():
+            s = s.intersection(person)
+
+        c += len(s)
+
+    assert 3268 == c
