775380dc255fcf25855539af6b8cd1f757757236
Tomas Aschan <1550920+tomasaschan@users.noreply.github.com>
2020-12-17 19:26:47 +0100

    Simplify and optimize 🐆

--- src/aoc/2020/dec17.py
+++ src/aoc/2020/dec17.py
@@ -1,105 +1,74 @@
-from collections import defaultdict
+from typing import Iterator
 
 
-def parse(input):
-    s = defaultdict(bool)
-    x, y = 0, 0
+def parse(input, dimensions):
+    assert dimensions == 3 or dimensions == 4
+    s = set()
     for y, line in enumerate(input.splitlines()):
         for x, c in enumerate(line):
             if c == "#":
-                s[(x, y, 0, 0)] = True
+                s.add((x, y, 0) if dimensions == 3 else (x, y, 0, 0))
 
-    return s, (0, 0, 0, 0), (x, y, 0, 0)
+    return s
 
 
-def get_puzzle_input():
+def get_puzzle_input(dimensions):
     with open("input/2020/dec17.txt") as f:
-        return parse(f.read())
+        return parse(f.read(), dimensions)
 
 
-def surroundings(x, y, z, w, dimensions=3):
-    assert dimensions == 3 or dimensions == 4
-    assert (w == 0) if (dimensions == 3) else True
+def surroundings(*coords: int) -> Iterator[tuple[int, ...]]:
+    assert len(coords) == 3 or len(coords) == 4
 
     for i in (-1, 0, 1):
         for j in (-1, 0, 1):
             for k in (-1, 0, 1):
-                if dimensions == 3:
-                    if i != 0 or j != 0 or k != 0:
-                        yield (x + i, y + j, z + k, 0)
+                if len(coords) == 3:
+                    yield (coords[0] + i, coords[1] + j, coords[2] + k)
                 else:
                     for l in (-1, 0, 1):  # noqa: E741
-                        if i != 0 or j != 0 or k != 0 or l != 0:  # noqa: E741
-                            yield (x + i, y + j, z + k, w + l)
+                        yield (
+                            coords[0] + i,
+                            coords[1] + j,
+                            coords[2] + k,
+                            coords[3] + l,
+                        )
 
 
-def active_surroundings(s, dimensions, x, y, z, w):
-    return sum(1 for p in surroundings(x, y, z, w, dimensions) if s[p])
+def active_surroundings(s: set[tuple[int, ...]], *c: int) -> int:
+    return sum(1 for p in surroundings(*c) if p in s and p != c)
 
 
-def frame_of_view(lo, hi, extra=0, dimensions=3):
-    assert dimensions == 3 or dimensions == 4
-    xlo, ylo, zlo, wlo = lo
-    xhi, yhi, zhi, whi = hi
-    for z in range(zlo - extra, zhi + 1 + extra):
-        for y in range(ylo - extra, yhi + 1 + extra):
-            for x in range(xlo - extra, xhi + 1 + extra):
-                if dimensions == 3:
-                    yield (x, y, z, 0)
-                else:
-                    for w in range(wlo - extra, whi + 1 + extra):
-                        yield (x, y, z, w)
+def points_of_interest(s: set[tuple[int, ...]]) -> set[tuple[int, ...]]:
+    def _all():
+        for p in s:
+            yield from surroundings(*p)
+
+    return set(_all())
 
 
-def evolve(s, lo, hi, dimensions=3):
-    nxt = defaultdict(bool)
-    xlo, ylo, zlo, wlo = lo
-    xhi, yhi, zhi, whi = hi
+def evolve(s: set[tuple[int, ...]]) -> set[tuple[int, ...]]:
+    nxt = set()
+    for c in points_of_interest(s):
+        if c in s and 2 <= active_surroundings(s, *c) <= 3:
+            nxt.add(c)
+        elif c not in s and active_surroundings(s, *c) == 3:
+            nxt.add(c)
 
-    for c in frame_of_view(lo, hi, extra=1, dimensions=dimensions):
-        if s[c]:
-            a = 2 <= active_surroundings(s, dimensions, *c) <= 3
-        else:
-            a = active_surroundings(s, dimensions, *c) == 3
-
-        nxt[c] = a
-
-        if a:
-            xlo = min(c[0], xlo)
-            ylo = min(c[1], ylo)
-            zlo = min(c[2], zlo)
-            wlo = min(c[3], wlo)
-            xhi = max(c[0], xhi)
-            yhi = max(c[1], yhi)
-            zhi = max(c[2], zhi)
-            whi = max(c[0], whi)
-
-    return nxt, (xlo, ylo, zlo, wlo), (xhi, yhi, zhi, whi)
-
-
-def draw_state(s, lo, hi):
-    xlo, ylo, zlo = lo
-    xhi, yhi, zhi = hi
-    for z in range(zlo, zhi + 1):
-        print(f"z={z}")
-        for y in range(ylo, yhi + 1):
-            for x in range(xlo, xhi + 1):
-                print("#" if s[(x, y, z)] else ".", end="")
-            print()
-        print()
+    return nxt
 
 
 def part_1():
-    s, lo, hi = get_puzzle_input()
+    s = get_puzzle_input(dimensions=3)
     for _ in range(6):
-        s, lo, hi = evolve(s, lo, hi, dimensions=3)
+        s = evolve(s)
 
-    assert 384 == sum(1 for v in s.values() if v)
+    assert 384 == len(s)
 
 
 def part_2():
-    s, lo, hi = get_puzzle_input()
+    s = get_puzzle_input(dimensions=4)
     for _ in range(6):
-        s, lo, hi = evolve(s, lo, hi, dimensions=4)
+        s = evolve(s)
 
-    assert 2012 == sum(1 for v in s.values() if v)
+    assert 2012 == len(s)
