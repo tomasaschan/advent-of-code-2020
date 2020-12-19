da6d78d92fee372224ac3a7e2a4f53a5de0d6543
Tomas Aschan <1550920+tomasaschan@users.noreply.github.com>
2020-12-19 23:00:39 +0100

    Dec 18 ⭐

---
+++ src/aoc/2020/dec18.py
@@ -0,0 +1,89 @@
+with open("input/2020/dec18.txt") as f:
+    input = f.read().splitlines()
+
+
+def tokenize(input: str):
+    if len(input) == 0:
+        return
+    if input[0] == "(":
+        yield ("OPEN",)
+        yield from tokenize(input[1:])
+    if input[0] == ")":
+        yield ("CLOSE",)
+        yield from tokenize(input[1:])
+    if input[0].isdigit():
+        i = 1
+        while i < len(input) and input[i].isdigit():
+            i += 1
+        yield ("LITERAL", int(input[0:i]))
+        yield from tokenize(input[i:])
+    if input[0] == " ":
+        yield from tokenize(input[1:])
+    if input[0] == "*":
+        yield ("MUL",)
+        yield from tokenize(input[1:])
+    if input[0] == "+":
+        yield ("ADD",)
+        yield from tokenize(input[1:])
+
+
+def parse_1(tokens):
+    # grammar:
+    #
+    # expression -> binary
+    # binary     -> term (("*" | "+") term)*
+    # term       -> NUMBER | "(" expression ")"
+
+    current = 0
+
+    def match(*ts):
+        nonlocal current
+        if current >= len(tokens):
+            return False
+        for t in ts:
+            if tokens[current][0] == t:
+                current += 1
+                return True
+
+        return False
+
+    def expression():
+        return binary()
+
+    def binary():
+        expr = term()
+
+        while match("ADD", "MUL"):
+            op = tokens[current - 1][0]
+            rhs = term()
+            expr = (op, expr, rhs)
+
+        return expr
+
+    def term():
+        if match("LITERAL"):
+            return ("LITERAL", tokens[current - 1][1])
+
+        if match("OPEN"):
+            expr = binary()
+            assert match("CLOSE")
+            return expr
+
+    return expression()
+
+
+def evaluate(expr):
+    if expr[0] == "LITERAL":
+        return expr[1]
+    if expr[0] == "ADD":
+        return evaluate(expr[1]) + evaluate(expr[2])
+    if expr[0] == "MUL":
+        return evaluate(expr[1]) * evaluate(expr[2])
+
+
+def compute_1(input):
+    return evaluate(parse_1(list(tokenize(input))))
+
+
+def part_1():
+    assert 1408133923393 == sum(compute_1(line) for line in input)
