051a3c0fb6c203cc19b73c0d1b46069930246221
Tomas Aschan <1550920+tomasaschan@users.noreply.github.com>
2020-12-22 15:38:24 +0100

    Use a deque for slightly better perf

--- src/aoc/2020/dec22.py
+++ src/aoc/2020/dec22.py
@@ -1,10 +1,13 @@
+from collections import deque
+
+
 def puzzle_input():
     with open("input/2020/dec22.txt") as f:
         player1, player2 = f.read().split("\n\n")
 
     return (
-        [int(card) for card in player1.splitlines()[1:]],
-        [int(card) for card in player2.splitlines()[1:]],
+        deque(int(card) for card in player1.splitlines()[1:]),
+        deque(int(card) for card in player2.splitlines()[1:]),
     )
 
 
@@ -18,7 +21,7 @@ def part_1():
     player1, player2 = puzzle_input()
 
     while player1 and player2:
-        p1, p2 = player1.pop(0), player2.pop(0)
+        p1, p2 = player1.popleft(), player2.popleft()
 
         if p1 > p2:
             player1.extend((p1, p2))
@@ -49,10 +52,14 @@ def play_recursive(player1, player2):
             else:
                 seen.add(stringed)
 
-            x, y = a.pop(0), b.pop(0)
+            x, y = a.popleft(), b.popleft()
 
             if len(a) >= x and len(b) >= y:
-                suba, subb = a[:x], b[:y]
+                suba, subb = a.copy(), b.copy()
+                while len(suba) > x:
+                    suba.pop()
+                while len(subb) > y:
+                    subb.pop()
                 (a_wins, _) = recurse(suba, subb, game=max(games) + 1)
                 if a_wins:
                     a.extend((x, y))
